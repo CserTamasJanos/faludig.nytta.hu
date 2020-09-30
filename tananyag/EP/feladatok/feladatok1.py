7292e89405f10b5ade05bc0391dd771a033c59ce
faludig <faludig@pm.me>
2020-10-01 00:39:51 +0200

    python feladatok upload

---
+++ tananyag/EP/feladatok/feladatok1.py
@@ -0,0 +1,186 @@
+# Kiíratás, adatbekérés billentyűzetről, változók, matematikai műveletek
+
+# 1. feladat (lakcim)
+
+irsz = input("Kérem adja meg az irányítószámát: ")
+varos = input("Kérem adja meg a várost: ")
+kozter = input("Kérem adja meg a közterület nevét: ")
+kjelleg = input("Kérem adja meg a közterület jellegét: ")
+hazsz = input("Kérem adja meg a házszámot: ")
+
+cim = f"{irsz} {varos}, {kozter} {kjelleg} {hazsz}"
+
+print(cim)
+
+
+# 2. feladat (nevek)
+
+vez1 = input("Kérek egy vezetéknevet: ")
+vez2 = input("Kérek még egy vezetéknevet: ")
+ker1 = input("Kérek egy keresztnevet: ")
+ker2 = input("Kérek még egy keresztnevet: ")
+
+print(f"{vez1} {ker1}")
+print(f"{vez1} {ker2}")
+print(f"{vez2} {ker1}")
+print(f"{vez2} {ker2}")
+
+
+# 3. feladat (fizetes)
+
+havi = int(input("Kérem adja meg a havi fizetést (Ft-ban): "))
+evi = havi * 12
+
+print(f"Az éves fizetés: {evi} Ft.")
+
+
+# 4. feladat
+
+arfolyam = float(input("Kérem adja meg az aktuális EUR/HUF árfolyamot: "))
+eur = float(input("Kérem adja meg mennyi EUR-t váltsunk: "))
+ft = eur * arfolyam
+
+print(f"Az átváltott összeg: {ft} Ft.")
+
+
+# 5. feladat (teglalap_ker_ter)
+
+a = float(input("Az a oldal hossza: "))
+b = float(input("A b oldal hossza: "))
+
+K = (a + b) * 2
+T = a * b
+
+print(f"A téglalap kerülete: {K}; területe: {T}")
+
+
+import math                                 # A következő két feladathoz.
+
+
+# 6. feladat (kor_ker_ter)
+
+r = float(input("Kör sugara: "))
+K = 2 * r * math.pi
+T = r ** 2 * math.pi
+
+print(f"A kör kerülete {K}; területe: {T}")
+
+
+# 7. feladat (pitagorasz)
+
+a = float(input("Egyik befogó: "))
+b = float(input("Másik befogó: "))
+c = math.sqrt(a ** 2 + b ** 2)
+
+print(f"Az átfogó: {c}")
+
+
+# 8. feladat (atlagsebesseg)
+
+d = float(input("A megtett út hossza (km): "))
+t = float(input("Eltelt idő (óra): "))
+v = d / t
+
+print(f"Az átlagsebesség {v} km/h.")
+
+
+# 9. feladat (uzemanyag)
+
+fogy = float(input("Az autó fogyasztása (l/100km): "))
+bar = float(input("A benzin ára (Ft/l): "))
+d = float(input("Az út hosza (km): "))
+
+kolts = (fogy * d * bar) / 100
+
+print(f"Az útiköltség: {kolts} Ft.")
+
+
+# 10. feladat (testtomegindex)
+
+tomeg = float(input("Testtömege (kg): "))
+magas = float(input("Magassága (cm): "))
+
+tindex = tomeg / ((magas / 100) ** 2)
+
+print(f"Testtömegindex (BMI): {tindex}")
+
+
+# 11. feladat (zoldseges)
+
+alma = 350
+szilva = 400
+szolo = 699
+
+print(f"alma: {alma} Ft/kg\nszilva: {szilva} Ft/kg\nszőlő: {szolo} Ft/kg")
+
+malm = float(input("Hány kg almát kér? "))
+mszi = float(input("Hány kg szilvát kér? "))
+mszo = float(input("Hány kg szőlőt kér? "))
+
+almar = malm * alma
+sziar = mszi * szilva
+szoar = mszo * szolo
+
+veg = almar + sziar + szoar
+
+print(f"{malm} kg alma = {almar} Ft.\n{mszi} kg szilva = {sziar} Ft.\n{mszo} kg szőlő = {szoar} Ft\nVégösszeg: {veg} Ft.")
+
+
+# 12. feladat (hordo)
+
+hordo = int(input("Mekkora a hordó (egész l)? "))
+kancso = int(input("Mekkora a kancsó (egész l)? "))
+
+tkancso = hordo // kancso
+marad = hordo % kancso
+hanyad = hordo / kancso
+
+print(f"{tkancso} teli kancsó mérhető ki, {marad} l víz marad, a hordó és a kancsó térfogatának hányadosa pedig {hanyad}.")
+
+
+# 13. feladat (bankjegy)
+
+print("Bankjegyautomata\n\nA legkisebb címlet 1000 Ft, a maximálisan felvehető összeg 300 000 Ft.\n")
+felvesz = int(input("Adja meg mekkora összeget kíván felvenni! "))
+
+tize = felvesz // 10000
+maradek = felvesz % 10000
+
+ote = maradek // 5000
+maradek = maradek % 5000
+
+e = maradek // 1000
+
+ell = tize * 10000 + ote * 5000 + e * 1000
+
+print(f"\nA kiadott bankjegyek:\n\n {tize:2} * 10 000 = {tize*10000:6}\n {ote:2} *  5 000 = {ote*5000:6}\n {e:2} *  1 000 = {e*1000:6}\n{'_' * 21}\n\nÖsszeg:{ell:14} Ft")
+
+
+# 14. feladat (uzemido) - Szerintem itt az üzemidőt ...nap ...óra ...perc ...másodperc formában gondolta a költő, de az eredeti feladat megoldása az alábbi.
+
+ump = int(input("Az eszköz üzemideje (mp): "))
+
+nap = ump // (3600 * 24)
+maradek = ump % (3600 * 24)
+
+ora = maradek // 3600
+
+mp = maradek % 3600
+
+print(f"Az eszköz üzemideje: {nap} nap {ora} óra {mp} másodperc.")
+
+
+# 15. feladat (utazasikoltseg)
+
+print("Utazási költségtérítés\n")
+
+ut = float(input("Add meg a megtett utat km-ben! "))
+fogy = float(input("Add meg az autó fogyasztását 100 km-re literben! "))
+ar = float(input("Add meg az üzemanyagárat ft-ban! "))
+
+uzemakolts = (ut * fogy * ar) / 100
+pluszkolts = int(ut > 100) * ut * 25                                # Azért így oldottam meg, hogy ne legyen benne if, mert ez a feladat nem az 'Elágazások' feladatosztály része.
+osszkolts = uzemakolts + pluszkolts
+
+print(f"\033[;7mKöltségtérítés: {osszkolts:.3f} ft.\033[0;0m")      # \033[;7m => megcseréli a konzol szövegének és hátterének a színét, \033[0;0m => alaphelyzetbe állítja.
+                                                                    # https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
