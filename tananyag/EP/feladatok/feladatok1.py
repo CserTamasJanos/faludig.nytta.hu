3d554d372fc6c150a36034e0f43c57b969cbfb79
faludig <faludig@pm.me>
2020-10-01 23:51:50 +0200

    feladatok megoldásai 16-30-ig

--- tananyag/EP/feladatok/feladatok1.py
+++ tananyag/EP/feladatok/feladatok1.py
@@ -187,3 +187,231 @@ osszkolts = uzemakolts + pluszkolts
 
 print(f"\033[;7mKöltségtérítés: {osszkolts:.3f} ft.\033[0;0m")      # \033[;7m => megcseréli a konzol szövegének és hátterének a színét, \033[0;0m => alaphelyzetbe állítja.
                                                                     # https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+
+
+#Elágazások
+
+# 16. feladat (fagy)
+
+hom = float(input("Mennyi a kinti hőmérséklet (C°)? "))
+
+if hom <= 0:
+    print("Odakint fagy van.")
+
+
+# 17. feladat (kerdes)
+
+valasz = input("Szeretsz programozni? ")
+
+if valasz.lower() == "igen":
+    print("Még sokra viszed az életben.")
+
+print("Viszont látásra!")                   # A feladatot úgy is lehet értelmezni, hogy az elköszönés az if-en belül van
+
+
+# 18. feladat (paritas)
+
+szam = int(input("Kérek egy egész számot: "))
+
+if szam % 2 == 0:
+    print("A szám páros.")
+else:
+    print("A szám páratlan.")
+
+
+# 19. feladat (oszthato3)
+
+szam = float(input("Kérek egy számot: "))
+
+if szam % 3 == 0:
+    print("A szám osztható 3-mal.")
+else:
+    print("A szám nem osztható 3-mal.")
+
+
+# 20. feladat (elojel)
+
+szam = complex(input("Kérek egy számot: "))               # Egyébként az összes olyan feladatnál, ahol nincs megadva, hogy milyen számot várunk,
+                                                          # float() helyett a complex() fv-t is használhatnánk, mert a komplex számok bővebb halmaz.
+                                                          # Mondjuk a <, > reláció nincs a komplexeknél értelmezve, meg oszthatóságnál is lehet, hogy meghal a python. :D
+
+if szam < 0:
+    print("A szám negatív.")
+elif szam > 0:
+    print("A szám pozitív.")
+else:
+    print("A szám nem pozitív és nem is negatív.")
+
+
+# 21. feladat (relacio)
+
+szam1 = float(input("Kérek egy számot! "))
+szam2 = float(input("Kérek még egy számot! "))
+
+if szam1 < szam2:
+    print(f"{szam1} < {szam2}")
+elif szam1 == szam2:
+    print(f"{szam1} = {szam2}")
+else:
+    print(f"{szam1} > {szam2}")
+
+
+# 22. feladat (kozotte)
+
+szam = float(input("Kérek egy számot! "))
+
+if -30 < szam < 40:
+    print("A szám -30 és 40 között van.")
+else:
+    print("A szám nincs a (-30,40) nyílt intervallumban.")
+
+
+# 23. feladat - kimaradt a feladat "neve"
+
+pont = float(input("A dolgozat pontszáma: "))
+
+if pont < 0 or pont > 100:
+    print("A pontszám nem értelmezhető.")
+elif 0 <= pont <= 42:
+    print("A dolgozat elégtelen.")
+elif pont <= 57:
+    print("A dolgozat elégséges.")
+elif pont <= 72:
+    print("A dolgozat közepes.")
+elif pont <= 87:
+    print("A dolgozat jó.")
+else:
+    print("A dolgozat jeles.")
+
+
+# 24. feladat (telepules)
+
+nev = input("A helység neve: ")
+leleksz = int(input("A helység lélekszáma: "))
+
+if leleksz <= 0:
+    print("Hibás adatbevitel")
+elif leleksz < 5000:
+    print(f"A(z) {nev} nevű település község.")
+elif leleksz < 20000:
+    print(f"A(z) {nev} nevű település kisváros.")
+elif leleksz < 100000:
+    print(f"A(z) {nev} nevű település kozépváros.")
+elif leleksz < 1000000:
+    print(f"A(z) {nev} nevű település nagyváros.")
+else:
+    print(f"A(z) {nev} nevű település metropolis.")
+
+
+# 25. feladat (muvelet)
+
+szam1 = float(input("Kérek egy számot: "))
+szam2 = float(input("Kérek mégy egy számot: "))
+oper = input("Kérek egy műveleti jelet (+,-,/,*): ")
+
+if oper == "+":
+    print(f"Az eredmény: {szam1 + szam2}")
+elif oper == "-":
+    print(f"Az eredmény: {szam1 - szam2}")
+elif oper == "/":
+    print(f"Az eredmény: {szam1 / szam2}")
+else:                                                   # Feltételezem, hogy az input jó - vagyis csak a '+', '-', '/', '*' karakterek egyike lehet.
+    print(f"Az eredmény: {szam1 * szam2}")
+
+
+# 26. feladat (osztalyzat)
+
+oszt = input("Év végi matematika jegy: ")
+
+if oszt == '1':
+    print("Elégtelen.")
+elif oszt == '2':
+    print("Elégséges.")
+elif oszt == '3':
+    print("Közepes.")
+elif oszt == '4':
+    print("Jó.")
+elif oszt == '5':
+    print("Jeles.")
+else:
+    print("Hibás adat.")
+
+
+# 27. feladat (hetnapjai)
+
+nap = input("A nap sorszáma: ")
+
+if nap == '1':
+    print("hétfő")
+elif nap == '2':
+    print("kedd")
+elif nap == '3':
+    print("szerda")
+elif nap == '4':
+    print("csütörtök")
+elif nap == '5':
+    print("péntek")
+elif nap == '6':
+    print("szombat")
+elif nap == '7':
+    print("vasárnap")
+else:
+    print("Hibás adat.")                # A 26-27-28-as feladatokban azért nem szórakozok listákkal, meg ciklusokkal, mert a feladatsor, illetve tananyag szerint "még nem vettük".
+
+
+# 28. feladat (honapok)
+
+honap = input("A nap sorszáma: ")
+
+if honap == '1':
+    print("január")
+elif honap == '2':
+    print("február")
+elif honap == '3':
+    print("március")
+elif honap == '4':
+    print("április")
+elif honap == '5':
+    print("május")
+elif honap == '6':
+    print("június")
+elif honap == '7':
+    print("július")
+elif honap == '8':
+    print("augusztus")
+elif honap == '9':
+    print("szeptember")
+elif honap == '10':
+    print("október")
+elif honap == '11':
+    print("november")
+elif honap == '12':
+    print("december")
+else:
+    print("Hibás adat.")
+
+
+# 29. feladat (vasarlas)
+
+ar = int(input("Az áru ára (Ft): "))
+db = int(input("Vásárolni kívánt mennyiség (db): "))
+penz = int(input("Elérhető összeg (Ft): "))
+
+ertek = ar * db
+
+if ertek <= penz:
+    print(f"Le tudjuk bonyolítani a vásárlást, és a megmaradt pénz {penz - ertek} Ft.")
+else:
+    print(f"A pénzünk {penz // ar} db termékre elég.")
+
+
+# 30. feladat (szokoev)
+
+ev = int(input("Milyen évet írunk? "))
+
+if ev % 4 == 0 and ev % 100 != 0:
+    print("Az év szökőév.")
+elif ev % 400 == 0:
+    print("Az év szökőév.")
+else:
+    print("Az év nem szökőév.")
